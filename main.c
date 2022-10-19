598f3d48baddcd785328f198f464dd197697ab95
DariyaDro <116110019+DariyaDro@users.noreply.github.com>
2022-10-19 23:35:19 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "functions.h"
+
+int main() {
+  vector v1;
+  v1.a=1; v1.b=2; v1.c=2.5;
+  vector v2;
+  v2.a=3; v2.b=4; v2.c=4.5;
+  vector v_res=sum(&v1, &v2);
+  printf("Sum: (%.3f, %.3f, %.3f)", v_res.a, v_res.b, v_res.c);
+  printf("\nSub: %.3f", sub(&v1, &v2));
+  return 0;
+}
