ab7dbbd86d979dfe50cae594c8358f01ff3f61b3
DariyaDro <116110019+DariyaDro@users.noreply.github.com>
2022-10-21 18:57:01 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -10,6 +10,20 @@ struct vector sum(struct vector* v1, struct vector* v2){
   res.c=v1->c+v2->c;
   return res;
 }
-double sub(struct vector* v1, struct vector*v2){ // скалярное произведение
+double dot(struct vector* v1, struct vector*v2){ // neaey?iia i?iecaaaaiea
   return v1->a*v2->a+v1->b*v2->b+v1->c*v2->c;
+}
+struct vector sub(struct vector* v1, struct vector* v2){
+  struct vector res;
+  res.a=v1->a-v2->a;
+  res.b=v1->b-v2->b;
+  res.c=v1->c-v2->c;
+  return res;
+}
+struct vector cross(struct vector* v1, struct vector* v2){
+	struct vector res;
+	res.a=v1->b*v2->c - v2->b*v1->c;
+	res.b = v1->c*v2->a - v1->a*v2->c;
+	res.c = v1->a*v2->b - v1->b*v2->a;
+	return res;
 }
