409517625175cb40942eff56d4946095a9101762
DariyaDro <116110019+DariyaDro@users.noreply.github.com>
2022-10-21 17:55:54 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -3,13 +3,13 @@ double a;
 double b;
 double c;
 };
-vector sum(vector* v1, vector* v2){
-  vector res;
+struct vector sum(struct vector* v1, struct vector* v2){
+  struct vector res;
   res.a=v1->a+v2->a;
   res.b=v1->b+v2->b;
   res.c=v1->c+v2->c;
   return res;
 }
-double sub(vector* v1, vector*v2){ // скалярное произведение
+double sub(struct vector* v1, struct vector*v2){ // скалярное произведение
   return v1->a*v2->a+v1->b*v2->b+v1->c*v2->c;
 }
