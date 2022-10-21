4dd2a31e49168f70211eba31f4c9583835ca2ff7
DariyaDro <116110019+DariyaDro@users.noreply.github.com>
2022-10-21 18:57:44 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,11 @@ int main() {
   v2.a=3; v2.b=4; v2.c=4.5;
   struct vector v_res=sum(&v1, &v2);
   printf("Sum: (%.3f, %.3f, %.3f)", v_res.a, v_res.b, v_res.c);
-  printf("\nSub: %.3f", sub(&v1, &v2));
+  v_res = sub(&v1, &v2);
+  printf("\nSub: (%.3f, %.3f, %.3f)", v_res.a, v_res.b, v_res.c);
+  printf("\nDot: %.3f", dot(&v1, &v2));
+  v_res = cross(&v1, &v2);
+  printf("\nCross: (%.3f, %.3f, %.3f)\n",  v_res.a, v_res.b, v_res.c);
   return 0;
 }
+
