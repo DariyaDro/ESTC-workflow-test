7324214696c6269157159f7520db4e539169d59f
DariyaDro <116110019+DariyaDro@users.noreply.github.com>
2022-10-21 17:57:03 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include "functions.h"
 
 int main() {
-  vector v1;
+  struct vector v1;
   v1.a=1; v1.b=2; v1.c=2.5;
-  vector v2;
+  struct vector v2;
   v2.a=3; v2.b=4; v2.c=4.5;
-  vector v_res=sum(&v1, &v2);
+  struct vector v_res=sum(&v1, &v2);
   printf("Sum: (%.3f, %.3f, %.3f)", v_res.a, v_res.b, v_res.c);
   printf("\nSub: %.3f", sub(&v1, &v2));
   return 0;
