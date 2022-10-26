6b821c915f9b9aa91200c3e7545d69398c78a7c3
DariyaDro <116110019+DariyaDro@users.noreply.github.com>
2022-10-26 12:56:22 +0300

    Update functions.h
    Отступы отредактированы.
    
    Close #1

--- functions.h
+++ functions.h
@@ -4,6 +4,9 @@ struct vector{
     double c;
 };
 struct vector sum(struct vector* v1, struct vector* v2);
+
 double dot(struct vector* v1, struct vector*v2);
+
 struct vector sub(struct vector* v1, struct vector* v2);
+
 struct vector cross(struct vector* v1, struct vector* v2);
