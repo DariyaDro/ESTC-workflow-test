869cd9b79d6945b5265d71cbc2f8419fae17434b
DariyaDro <116110019+DariyaDro@users.noreply.github.com>
2022-10-26 12:16:09 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,29 +1,9 @@
 struct vector{
-double a;
-double b;
-double c;
+    double a;
+    double b;
+    double c;
 };
-struct vector sum(struct vector* v1, struct vector* v2){
-  struct vector res;
-  res.a=v1->a+v2->a;
-  res.b=v1->b+v2->b;
-  res.c=v1->c+v2->c;
-  return res;
-}
-double dot(struct vector* v1, struct vector*v2){ // neaey?iia i?iecaaaaiea
-  return v1->a*v2->a+v1->b*v2->b+v1->c*v2->c;
-}
-struct vector sub(struct vector* v1, struct vector* v2){
-  struct vector res;
-  res.a=v1->a-v2->a;
-  res.b=v1->b-v2->b;
-  res.c=v1->c-v2->c;
-  return res;
-}
-struct vector cross(struct vector* v1, struct vector* v2){
-	struct vector res;
-	res.a=v1->b*v2->c - v2->b*v1->c;
-	res.b = v1->c*v2->a - v1->a*v2->c;
-	res.c = v1->a*v2->b - v1->b*v2->a;
-	return res;
-}
+struct vector sum(struct vector* v1, struct vector* v2);
+double dot(struct vector* v1, struct vector*v2);
+struct vector sub(struct vector* v1, struct vector* v2);
+struct vector cross(struct vector* v1, struct vector* v2);
