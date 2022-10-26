052b77dd4d636856bc8f9dedfb842c98f4dd1a96
DariyaDro <116110019+DariyaDro@users.noreply.github.com>
2022-10-26 12:14:48 +0300

    Create functions.c

---
+++ functions.c
@@ -0,0 +1,26 @@
+#include "functions.h"
+
+struct vector sum(struct vector* v1, struct vector* v2){
+    struct vector res;
+    res.a=v1->a+v2->a;
+    res.b=v1->b+v2->b;
+    res.c=v1->c+v2->c;
+    return res;
+}
+double dot(struct vector* v1, struct vector*v2){
+    return v1->a*v2->a+v1->b*v2->b+v1->c*v2->c;
+}
+struct vector sub(struct vector* v1, struct vector* v2){
+    struct vector res;
+    res.a=v1->a-v2->a;
+    res.b=v1->b-v2->b;
+    res.c=v1->c-v2->c;
+    return res;
+}
+struct vector cross(struct vector* v1, struct vector* v2){
+	  struct vector res;
+	  res.a=v1->b*v2->c - v2->b*v1->c;
+	  res.b = v1->c*v2->a - v1->a*v2->c;
+	  res.c = v1->a*v2->b - v1->b*v2->a;
+	  return res;
+}
